7f712764c55ce35d12950eeeeb21d3814b21374a
kassded <124594552+kassded@users.noreply.github.com>
2023-09-03 18:53:38 +0800

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_t982_ar31a8.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_t982_ar31a8-user \
+    omni_t982_ar31a8-userdebug \
+    omni_t982_ar31a8-eng
