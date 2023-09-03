eae27c0079590e4fc3472d9d908c5dff73485378
kassded <124594552+kassded@users.noreply.github.com>
2023-09-03 22:37:03 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -6,25 +6,3 @@
 #
 
 LOCAL_PATH := device/tcl/t982_ar31a8
-# A/B
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
-
-# Boot control HAL
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
-
-PRODUCT_PACKAGES += \
-    bootctrl.sc2
-
-
-PRODUCT_PACKAGES += \
-    otapreopt_script \
-    cppreopts.sh \
-    update_engine \
-    update_verifier \
-    update_engine_sideload
