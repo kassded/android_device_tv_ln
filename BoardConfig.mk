7802393dccdcea9cddf8c9fd186f35249e418240
kassded <124594552+kassded@users.noreply.github.com>
2024-02-25 16:44:09 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -96,8 +96,8 @@ VENDOR_SECURITY_PATCH := 2099-12-31
 PLATFORM_VERSION := 16.1.0
 
 # TWRP Configuration
-#TW_THEME := landscape_hdpi
-TW_THEME := portrait_hdpi
+TW_THEME := landscape_hdpi
+#TW_THEME := portrait_hdpi
 #TARGET_SCREEN_HEIGHT := 720
 #TARGET_SCREEN_WIDTH := 1280
 TW_EXTRA_LANGUAGES := true
