76ef5b7f71ee14b3a12f8633598f65ce880aa9f0
kassded <124594552+kassded@users.noreply.github.com>
2024-02-26 17:56:19 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -101,6 +101,7 @@ PLATFORM_VERSION := 16.1.0
 TW_THEME := landscape_hdpi
 #TW_THEME := portrait_hdpi
 TW_EXTRA_LANGUAGES := true
+TW_DEFAULT_LANGUAGE := zh_CN  #设置默认语言为简体中文
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
