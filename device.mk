09643257e4483b0ceaadb817694707a6881ed7ff
kassded <124594552+kassded@users.noreply.github.com>
2024-03-16 21:11:45 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,11 +1,28 @@
 #
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+
 LOCAL_PATH := device/tcl/t982_ar31a8
+ALLOW_MISSING_DEPENDENCIES := true
+
 # A/B
+AB_OTA_UPDATER := true
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+AB_OTA_PARTITIONS += \
+    system_ext \
+    recovery \
+    vendor \
+    vbmeta \
+    odm \
+    system \
+    boot \
+    vbmeta_system \
+    product
+
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
@@ -14,19 +31,23 @@ AB_OTA_POSTINSTALL_CONFIG += \
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-impl.recovery \
-    bootctrl.sc2 \
-    bootctrl.sc2.recovery \
-    android.hardware.boot@1.0-service
-
-PRODUCT_PACKAGES += \
-    bootctrl.sc2
-
-
-PRODUCT_PACKAGES += \
     otapreopt_script \
     cppreopts.sh \
     update_engine \
     update_verifier \
     update_engine_sideload
+
+# Boot control
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-service \
+    bootctrl.sc2 \
+
+PRODUCT_PACKAGES_DEBUG += \
+    bootctl
+
+PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+    bootctrl.sc2 \
+    libcutils \
+    libgptutils \
+    libz
