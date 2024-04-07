c05ff86173f5d8b994bb1bbb741fa92e45af68c6
kassded <124594552+kassded@users.noreply.github.com>
2024-04-08 04:29:19 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,8 +8,5 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_t982_ar31a8.mk
 
-COMMON_LUNCH_CHOICES := \
-    twrp_t982_ar31a8-user \
-    twrp_t982_ar31a8-userdebug \
-    twrp_t982_ar31a8-eng
+COMMON_LUNCH_CHOICES := twrp_t982_ar31a8-eng
 
